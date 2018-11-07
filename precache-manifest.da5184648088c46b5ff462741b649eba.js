ca50f564e60227286f8b8a97f8ad54e68d6b9820
Derek <26776606+fissioner@users.noreply.github.com>
2018-11-06 19:29:58 -0700

    initial commit

---
+++ precache-manifest.da5184648088c46b5ff462741b649eba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c60b1c91470dc054465d",
+    "url": "/static/js/main.c60b1c91.chunk.js"
+  },
+  {
+    "revision": "5c508111ab46ce9311c6",
+    "url": "/static/js/1.5c508111.chunk.js"
+  },
+  {
+    "revision": "c60b1c91470dc054465d",
+    "url": "/static/css/main.bf1921ae.chunk.css"
+  },
+  {
+    "revision": "621616b703cf2cb4804873a40796d5ce",
+    "url": "/index.html"
+  }
+];
