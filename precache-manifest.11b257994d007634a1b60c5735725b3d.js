f76ccae61873b8164153ec04d6fb1470e72ee520
Derek <26776606+fissioner@users.noreply.github.com>
2018-11-08 00:50:18 -0700

    Update Build

---
+++ precache-manifest.11b257994d007634a1b60c5735725b3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6b9f2aaffd45bb103745",
+    "url": "/static/js/main.6b9f2aaf.chunk.js"
+  },
+  {
+    "revision": "3690d6bd72920f32ca68",
+    "url": "/static/js/1.3690d6bd.chunk.js"
+  },
+  {
+    "revision": "6b9f2aaffd45bb103745",
+    "url": "/static/css/main.bf1921ae.chunk.css"
+  },
+  {
+    "revision": "18dcefaf87e482cf67e516a5c1865dd1",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c418f5da974003d9db90548d8ca860f7.js"
+  "/precache-manifest.11b257994d007634a1b60c5735725b3d.js"
 );
 
 workbox.clientsClaim();
