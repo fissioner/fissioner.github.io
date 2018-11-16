8cab1fd1cb55b7b807f9f5c81943cd0313d901d7
Derek <26776606+fissioner@users.noreply.github.com>
2018-11-16 13:03:49 -0700

    Refactoring

---
+++ employee_manager/precache-manifest.bd3f9ea1779eb957988dc408f6725bf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fe9ef6bb7b0279952575",
+    "url": "/static/js/main.fe9ef6bb.chunk.js"
+  },
+  {
+    "revision": "c4bcaa90690e1474a532",
+    "url": "/static/js/1.c4bcaa90.chunk.js"
+  },
+  {
+    "revision": "fe9ef6bb7b0279952575",
+    "url": "/static/css/main.01e54643.chunk.css"
+  },
+  {
+    "revision": "6862062070a20a2d35191a8fbbf7062b",
+    "url": "/index.html"
+  }
+];
