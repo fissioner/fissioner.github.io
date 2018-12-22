b00f2ccc8a77cfb17a494483c93c28f8b5a8c07f
Derek <26776606+fissioner@users.noreply.github.com>
2018-12-21 23:00:36 -0700

    added solo play

---
+++ precache-manifest.03d2fdbe5c4af3960b323bdd9d1ab5d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2ac0163502ba9c4e0999",
+    "url": "/static/js/main.2ac01635.chunk.js"
+  },
+  {
+    "revision": "317aa25aa89aca199916",
+    "url": "/static/js/1.317aa25a.chunk.js"
+  },
+  {
+    "revision": "2ac0163502ba9c4e0999",
+    "url": "/static/css/main.9d283da0.chunk.css"
+  },
+  {
+    "revision": "42aa44b505d7e64d349b8e6777a4f97f",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d8c81b5f3a1e7d71f7ed573f9401160.js"
+  "/precache-manifest.03d2fdbe5c4af3960b323bdd9d1ab5d5.js"
 );
 
 workbox.clientsClaim();
